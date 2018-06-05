7694a4b7fb43c9a7519635d9cd756585ac38d323
Dillon <dillonmorris91@gmail.com>
2018-06-04 23:53:22 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Typewise</h1>
+          <p className="App-subheader">
+            A curated assortment of the best Google Fonts.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
